e2d007951e528fe086da8d6b70d2f3949110a272
Nirlan Souza <nirlan858@gmail.com>
2021-05-21 18:29:17 -0300

    Criação de uma variável que explica a condicional

--- adivinhacao.c
+++ adivinhacao.c
@@ -10,24 +10,24 @@ int main() {
 	printf("******************************************\n");
 
 	int numerosecreto = 42;
-
 	int chute;
 
 	printf("Qual é o seu chute? ");
 	scanf("%d", &chute);
 	printf("Seu chute foi %d\n", chute);
 
-	if(numerosecreto == chute) {
+	int acertou = (chute == numerosecreto);
+
+	if(acertou) {
 		printf("Parabéns! Você acertou!\n");
 		printf("Jogue de novo, você é um bom jogador!\n");
 	} 
 	else {
-
-		if(chute > numerosecreto) {
+		
+		int maior = (chute > numerosecreto);
+		if(maior) {
 			printf("Seu número foi maior que o número secreto\n");
-		}
-
-		if(chute < numerosecreto) {
+		} else {
 			printf("Seu chute foi menor que o número secreto\n");
 		}
 	}
