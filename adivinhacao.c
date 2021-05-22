583cc209b4aac6ecff2e30f28243b6da7c5338f1
Nirlan Souza <nirlan858@gmail.com>
2021-05-22 07:46:56 -0300

    Implementado o for loop e declarado o numero de tentativas como uma constante

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main() {
 
 	// imprime o cabeçalho do nosso jogo
@@ -12,24 +14,31 @@ int main() {
 	int numerosecreto = 42;
 	int chute;
 
-	printf("Qual é o seu chute? ");
-	scanf("%d", &chute);
-	printf("Seu chute foi %d\n", chute);
+	for(int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
 
-	int acertou = (chute == numerosecreto);
+		printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+		printf("Qual é o seu chute? ");	
+			
+		scanf("%d", &chute);
+		printf("Seu chute foi %d\n", chute);
 
-	if(acertou) {
-		printf("Parabéns! Você acertou!\n");
-		printf("Jogue de novo, você é um bom jogador!\n");
-	} 
-	else {
-		
-		int maior = (chute > numerosecreto);
-		if(maior) {
-			printf("Seu número foi maior que o número secreto\n");
-		} else {
-			printf("Seu chute foi menor que o número secreto\n");
+		int acertou = (chute == numerosecreto);
+
+		if(acertou) {
+			printf("Parabéns! Você acertou!\n");
+			printf("Jogue de novo, você é um bom jogador!\n");
+
+			break;
+		} 
+		else {
+			
+			int maior = (chute > numerosecreto);
+			if(maior) {
+				printf("Seu número foi maior que o número secreto\n");
+			} else {			
+				printf("Seu chute foi menor que o número secreto\n");
+			}		
 		}
 	}
-
+	printf("Fim de jogo!");
 }
